7c87e071d033e8f3735790bc3e9691bd8f1f22d9
Yuliya Bondarenko <yulia-pingvin@mail.ru>
2024-05-30 12:49:27 +0700

    create and delete card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const content = document.querySelector('.content');
+const cardsConteiner = content.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function createCard(element, deleteCard) {
+    let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const placesItem = evt.target.closest('.places__item');
+    placesItem.remove();
+    deleteCard(evt);
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+    const cardElement = createCard(element, deleteCard);
+    cardsConteiner.append(cardElement);
+    createCard(element, deleteCard);
+})
